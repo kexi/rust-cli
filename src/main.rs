35d94d3a664e50b61aa70360d5df8471af7b3760
Kei Nakayama <kei.of.nakayama@gmail.com>
2023-01-22 14:24:02 +0900

    fix

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::fs::{File};
-use std::io::{BufRead, BufReader};
+use std::io::{stdin, BufRead, BufReader};
 use clap::Parser;
 
 #[derive(Parser, Debug)]
@@ -24,14 +24,34 @@ fn main() {
         let reader = BufReader::new(f);
         run(reader, opts.verbose);
     } else {
-        // ファイルを指定しなかった場合
-        println!("No file is specified");
+        let stdin = stdin();
+        let reader = stdin.lock();
+        run(reader, opts.verbose);
     }
 }
 
-fn run(reader: BufReader<File>, verbose: bool) {
+fn run<R: BufRead>(reader: R, verbose: bool) {
+    let calc = RpnCalculator::new(verbose);
+
     for line in reader.lines() {
         let line = line.unwrap();
-        println!("{}", line);
+        let answer = calc.eval(&line);
+        println!("{}", answer);
+    }
+}
+
+struct RpnCalculator(bool);
+impl RpnCalculator {
+    pub fn new(verbose: bool) -> Self {
+        Self(verbose)
+    }
+
+    pub fn eval(&self, formula: &str) -> i32 {
+        let mut tokens = formula.split_whitespace().rev().collect::<Vec<_>>();
+        self.eval_inner(&mut tokens)
+    }
+
+    fn eval_inner(&self, tokens: &mut Vec<&str>) -> i32 {
+        0
     }
 }
