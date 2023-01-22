ce315feb92137d4d685cf8672723ba7fd0d9e65b
Kei Nakayama <kei.of.nakayama@gmail.com>
2023-01-22 14:31:20 +0900

    fix

--- src/main.rs
+++ src/main.rs
@@ -52,6 +52,30 @@ impl RpnCalculator {
     }
 
     fn eval_inner(&self, tokens: &mut Vec<&str>) -> i32 {
-        0
+        let mut stack = Vec::new();
+
+        while let Some(token) = tokens.pop() {
+            if let Ok(x) = token.parse::<i32>() {
+                stack.push(x);
+            } else {
+                let y = stack.pop().expect("invalid syntax");
+                let x = stack.pop().expect("invalid syntax");
+                let res = match token {
+                    "+" => x + y,
+                    "-" => x - y,
+                    "*" => x * y,
+                    "/" => x / y,
+                    "%" => x % y,
+                    _ => panic!("invalid token"),
+                };
+                stack.push(res);
+            }
+        }
+
+        if stack.len() == 1 {
+            stack[0]
+        } else {
+            panic!("invalid syntax")
+        }
     }
 }
